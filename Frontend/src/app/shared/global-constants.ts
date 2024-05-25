d93ce6d33dc4d71f3bcc0863f99b1cb8e9168ac3
ahmad nazran <ahmadnazran@graduate.utm.my>
2024-05-25 14:01:06 +0800

    fixing login and register scss

--- Frontend/src/app/shared/global-constants.ts
+++ Frontend/src/app/shared/global-constants.ts
@@ -6,6 +6,6 @@ export class GlobalConstants {
     //Regex
     public static usernameRegex: string = "^[a-z0-9._]{5,20}$";
     public static emailRegex: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
-    public static passwordRegex: string = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\w\W]{5,20}$";
+    public static passwordRegex: string = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{5,20}$";
     public static phoneRegex: string = "^[0-9]{10}$";
 }
